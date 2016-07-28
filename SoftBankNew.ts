18dc04ddb7210ee1242f033bdbe65cddf9487ddd
Dhruv Tapasvi <Dhruv.Tapasvi@softwire.com>
2016-07-28 12:35:38 +0100

    Markups made

--- SoftBankNew.ts
+++ SoftBankNew.ts
@@ -1,114 +1,125 @@
-// Logging preamble
+var readlineSync = require('readline-sync')
 var winston = require('winston');
+
+// LOGGING:
+
 winston.level = 'debug';
 winston.add(winston.transports.File, {filename: 'SoftBankNew2Problems.log' });
 winston.remove(winston.transports.Console);
 
-interface Account { // Interface for each person's account
-    net :number;
+// ITNERFACES:
+
+// Interface for each person's account
+interface Account {
+    balance :number;
     transactions :Transaction[];
 }
 
-interface Transaction { // Interface for all the transactions of each person.
-    credit :boolean;
-    amount :number
+// Interface for all the transactions of each person.
+interface Transaction { 
+    from :string; //Other person involved in the transaction
+    to: string;
+    amount :number;
     date :string;
     narrative :string;
 }
 
-function printPersonsTransactions(person :string) {// Prints all the transactions for a given person
-    if (peopleAccounts.hasOwnProperty(person)) {
+// METHODS:
+
+// Inputs the file and parse it into an array of lines:
+function inputParse() :string[] {
+    var fs = require('fs');
+    var text = fs.readFileSync('DodgyTransactions2015.csv','utf8',(err,data) => {
+        if (err) throw err;
+        return data;
+    });
+    return text.split('\r\n');
+};
+
+// Parses the transactionLine and alter both the creditor and the debtor
+function processTransaction(transactionLine: string) :void {
+    var details = transactionLine.split(',');
+    var dateString = details[0]
+    var fromPerson = details[1];
+    var toPerson = details[2];
+    var narr = details[3];
+    var amt = parseFloat(details[4]);
+    if (isNaN(amt)) {
+        winston.log('error', 'You have entered an invalid trasaction, becuase you have entered ' + details[4] + ' as the amount, which is not valid. This transaction will not be recorded.');
+    } else if (!/\b(0[1-9]|1[1-9]|2[1-9]|30|31)\/(0[1-9]|1[0-2])\/[1-2][0-9][0-9][0-9]\b/.test(dateString)) {
+        winston.log('error', 'You have entered an invalid trasaction, becuase you have entered ' + dateString + ' as the date, which is not a valid format. This transaction will not be recorded. For future reference, please enter the date in a dd/mm/yyyy format');
+    } else {
+        var thisTransaction = { from: fromPerson, to: toPerson, amount: amt, date: dateString, narrative: narr};
+        alterAccount(toPerson,amt,thisTransaction); // Add credit to 'toPerson'
+        alterAccount(fromPerson,-1*amt,thisTransaction); // Remove credit from 'fromPerson'
+    }
+};
+
+// Function updates an account with the given transaction details
+function alterAccount(accountPerson :string, amt :number, trans :Transaction) :void {
+    if(!accounts.hasOwnProperty(accountPerson)) {
+        accounts[accountPerson] = {balance: 0.0, transactions: []};
+    }
+    accounts[accountPerson].balance += amt;
+    accounts[accountPerson].transactions.push(trans);
+};
+
+// Prints everyone's account balances
+function printPeopleBalances() :void {
+    for (var person in accounts) {
+        console.log(person + ': ' + accounts[person].balance);
+    }
+};
+
+// Prints all the transactions for a given person
+function printPersonsTransactions(person :string) :void {
+    if (accounts.hasOwnProperty(person)) {
         console.log(person + ':');
-        var bal = peopleAccounts[person].net;
-        console.log('Your balance is: ' + bal + '.');
-        var pT = peopleAccounts[person].transactions;
-        var pTlen = pT.length;
-        for (var i=0; i<pTlen; i++) {
+        console.log('Balance is: ' + accounts[person].balance + '.');
+        var pT = accounts[person].transactions;
+        for (var i=0; i<pT.length; i++) {
             var transaction = pT[i];
-            if (pT[i].credit) {
-                console.log('Credit of ' + transaction.amount + ', dated ' + transaction.date + ', for reason: ' + transaction.narrative + '.');
-            } else {
-                console.log('Debit of ' + transaction.amount + ', dated ' + transaction.date + ', for reason: ' + transaction.narrative + '.');
-            }
+            console.log('From ' + transaction.from + ' to ' + transaction.to + ', dated ' + transaction.date + ', totalling ' + transaction.amount + ' for reason: ' + transaction.narrative + '.');
         }
     } else {
         console.log('Person not found.');
     }
 };
 
-var peopleAccounts = {} // Dictionary of people, with their names as the keys to access their accounts
-
-// Input the file and parse it into an array of lines:
-var fs = require('fs');
-var text = fs.readFileSync('DodgyTransactions2015.csv','utf8',(err,data) => {
-    if (err) throw err;
-    return data;
-})
-var lines = text.split('\r\n');
-
-// Process each line to produce all the accounts and update their transactions accordingly
-var len = lines.length;
-for (var i=1; i<len-1; i++) { // From second line to second last. There is a header line first, and an empty line last.
-    var details = lines[i].split(',');
-    var dateString = details[0]
-    var fromPerson = details[1];
-    var to = details[2];
-    var narr = details[3];
-    var amt = parseFloat(details[4]);
-
-    var dateFormat = /\b(0[1-9]|1[1-9]|2[1-9]|30|31)\/(0[1-9]|1[0-2])\/[1-2][0-9][0-9][0-9]\b/;
-
-
-    if (isNaN(amt)) {
-        winston.log('error', 'You have entered an invalid trasaction, becuase you have entered ' + details[4] + ' as the amount, which is not valid. This transaction will not be recorded.');
-    } else if (!dateFormat.test(dateString)) {
-        winston.log('error', 'You have entered an invalid trasaction, becuase you have entered ' + dateString + ' as the date, which is not a valid format. This transaction will not be recorded. For future reference, please enter the date in a dd/mm/yyyy format');
-    } 
-    else {
-    if (!peopleAccounts.hasOwnProperty(fromPerson)) {
-        peopleAccounts[fromPerson] = {net: 0.0, transactions: []};
+// Offers command line input for the program
+function commandLineInstructions() :void {
+    while (command != 'Quit') {
+        var command = readlineSync.question("Enter a command. 'List_All' to show everyone's balances. 'List_Person' to list the transactions of a person, chosen later through a menu. 'Quit' to quit: ");
+        console.log('You command was: ' + command);
+        switch (command) {
+            case 'List_All':
+                printPeopleBalances();
+                console.log('Done!');
+                break;
+            case 'List_Person':
+                var personName = readlineSync.question('Whose Account do you wish to see: ');
+                printPersonsTransactions(personName);
+                console.log('Done!');
+                break;
+            case 'Quit':
+                console.log('Quit successful');
+                break;
+            default:
+                console.log('Sorry, invalid input. Please try again.');
+        }
     }
-    peopleAccounts[fromPerson].net -= amt;
-    peopleAccounts[fromPerson].transactions.push({
-        credit: false, amount: amt, date: details[0], narrative: details[3]
-    })
+};
 
-    if (!peopleAccounts.hasOwnProperty(to)) {
-        peopleAccounts[to] = {net: 0.0, transactions: []};
-    }
-    peopleAccounts[to].net += amt;
-    peopleAccounts[to].transactions.push({
-        credit: true, amount: amt, date: details[0], narrative: details[3]
-    })
-    }
-}
+// Runs the program
+function run() :void {
+    var lines = inputParse();
+    for (var i=1; i<lines.length-1; i++) { // For some reason the last line of this file is blank, hence we need 'lines.length-1'
+        processTransaction(lines[i]);
+    };
+    commandLineInstructions();
+};
 
-function printPeopleBalances() { // Prints everyone's account balances
-    for (var person in peopleAccounts) {
-        console.log(person + ': ' + peopleAccounts[person].net);
-    }
-}
+// SCRIPT:
 
-// Ask the user for command line instructions
-var readlineSync = require('readline-sync');
-var command = '';
-var personName = '';
-while (command != 'Quit') {
-    command = readlineSync.question("Enter a command. 'List_All' to show everyone's balances. 'List_Person' to list the transactions of a person, chosen later through a menu. 'Quit' to quit: ");
-    switch (command) {
-        case 'List_All':
-            printPeopleBalances();
-            console.log();
-            break;
-        case 'List_Person':
-            personName = readlineSync.question('Whose Account do you wish to see: ');
-            printPersonsTransactions(personName);
-            console.log();
-            break;
-        case 'Quit':
-            console.log('Quit successful');
-            break;
-        default:
-            console.log('Sorry, invalid input. Please try again.');
-    }
-}
+var accounts = {}; // Dictionary of people, with their names as the keys to access their accounts
+run();
